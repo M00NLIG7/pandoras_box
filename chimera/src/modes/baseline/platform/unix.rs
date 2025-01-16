d458f62429e1706cad802b2fc8670b9e3875f1e6
M00NLIG7 <test@test.com>
2025-01-16 15:54:31 -0800

    Made baseline not panic

--- chimera/src/modes/baseline/platform/unix.rs
+++ chimera/src/modes/baseline/platform/unix.rs
@@ -1,3 +1,6 @@
 use crate::error::Result;
 
-pub async fn establish_baseline() -> Result<()> {todo!()}
+pub async fn establish_baseline() -> Result<()> {
+    println!("Establishing baseline for Unix");
+    Ok(())
+}
