6f5231c0464147487452dd4416051b302139cdea
M00NLIG7 <57321738+M00NLIG7@users.noreply.github.com>
2026-04-01 17:02:14 -0700

    refactor(runtime): remove legacy orchestrator modules

--- pandoras_box/src/lib.rs
+++ pandoras_box/src/lib.rs
@@ -1,12 +1,6 @@
-#[cfg(feature = "legacy")]
-pub mod communicator;
 pub mod config;
 pub mod enumerator;
 pub mod error;
-#[cfg(feature = "legacy")]
-pub mod logging;
-#[cfg(feature = "legacy")]
-pub mod orchestrator;
 pub mod runtime;
 pub mod ttl;
 
